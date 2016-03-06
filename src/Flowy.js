ac99895cab254888d697ac47e3e52f2b8e767d29
Justin Jose <justinjose999@gmail.com>
2016-03-06 20:57:48 +0530

    Completed If elseif else, modified object structure to reduce coupling

--- src/Flowy.js
+++ src/Flowy.js
@@ -1,10 +1,30 @@
-Flowy = function() {
-  var chart = {
-    block: new Block('Begin', 'Algorithm')
+var Block = function(desc, type) {
+  return {
+    tree:[],
+    desc: desc,
+    type: type
   };
+}
+
+Flowy = function() {
+
+  var chart = {
+    block: new Block("Begin", "Algorithm")
+  }
 
   var topBlock = [];
   
+  this.desc = function(desc){
+    return desc ? chart.block.desc = desc : chart.block.desc;
+  };
+
+  this.type = function(type){
+    return type ? chart.block.type = type : chart.block.type;
+  }
+  
+  this.push = function(block) {
+    this.tree.push(block);
+  }
   
   this.getBlock = function() {
     return chart.block;
@@ -17,7 +37,7 @@ Flowy = function() {
   this.tree = chart.block.tree;
   
   this.pushBlock = function(block, updateTop) {
-    chart.block.push(block);
+    this.push(block);
     updateTop ? this.tree = function()
     {
       topBlock.push(chart.block);
@@ -31,41 +51,41 @@ Flowy = function() {
     {
       chart.block =  topBlock.pop()
       return chart.block.tree;
-    }()  : null;
-  };
-  
-  this.else = function(desc) {
-    var type = chart.block.type();
-    if(['If','ElseIf'].indexOf(type) != -1 ) {  
-      var block = this.createBlock(desc, 'Else');
-      this.end();
-      this.pushBlock(block, true);
-      return this;
-    } else {
-      throw "Else without an If";
-    }
+    }(): null;
   };
 
-  this.elseif = function(desc) {
-    var type = chart.block.type();
-    if(['If','ElseIf'].indexOf(type) != -1 ) {
-      var block = this.createBlock(desc, 'ElseIf');
+  this.elseif = function(desc, elseType) {
+    elseType = (elseType || 'ElseIf');
+    if(['If','ElseIf'].indexOf(chart.block.type) != -1 ) {  
+      var block = this.createBlock(desc, elseType);
       this.end();
       this.pushBlock(block, true);
       return this;
     } else {
-      throw new Error('Else without an If')
+      throw 'Else without an If';
     }  
+  };
+
+  this.getJSON = function() {
+    var getObjectRec = function(block) {
+      
+      if(block.tree)
+      block.tree = block.tree.map(function(_) {
+                     _ = getObjectRec(_);
+                     return _; 
+                   });
+      return block;
+    };
+
+    while(this.end() != null);
+    var block = getObjectRec(this.getBlock());
+    return block;
   };
 };
 
 Flowy.prototype.createBlock = function(desc, type) {
   var block = new Block(desc, type);
   return block;
-}
-
-Flowy.prototype.getJSON = function() {
-  return this.getBlock();
 };
 
 Flowy.prototype.statement = function(desc){
@@ -90,34 +110,6 @@ Flowy.prototype.do = function(desc) {
   return this.then(desc);
 };
 
-
-
-
-
-var Block = function(blockDesc, blockType) {
-  var blockObject = {
-    tree: [],
-    desc: blockDesc,
-    type: blockType
-  };
-
-  this.getObject = function() {
-    return blockObject;
-  };
-  
-  this.desc = function(desc){
-    return desc ? blockObject.desc = desc : blockObject.desc;
-  };
-
-  this.type = function(type){
-    return type ? blockObject.type = type : blockObject.type;
-  }
-  
-  this.tree =  blockObject.tree;
-  
-  this.push = function(block) {
-    blockObject.tree.push(block);
-  };
-};
-
-  
+Flowy.prototype.else = function(desc) {
+  return this.elseif(desc, 'Else');
+}; 
--- src/indexSpec.js
+++ src/indexSpec.js
@@ -1,4 +1,5 @@
 /*global  */
+'use strict'
 var expect = require('chai').expect,
     assert = require('chai').assert;
 require('./index');
@@ -8,29 +9,27 @@ describe('flowy', function() {
   beforeEach(function() {
     flowy = new Flowy();
   })
-  it('init test', function() {
-    expect(true).to.be.true;
-  });
-  it('Should create Flowy class on .getJSON', function() {
-    
+  
+  it('Should return Top block on .getJSON', function() {
     expect(flowy.getJSON().tree).to.be.empty;
-    expect(flowy.getJSON().desc()).to.be.equal('Begin');
+    expect(flowy.getJSON().desc).to.be.equal('Begin');
+    expect(flowy.getJSON().type).to.be.equal('Algorithm');
   });
 
   it('Should create a block with desc "test"', function() {
     var block = flowy.createBlock('test');
-    expect(block.desc()).to.be.equal('test');
+    expect(block.desc).to.be.equal('test');
   });
 
   it('.desc should return "test"', function() {
     var block = flowy.createBlock('test');
-    expect(block.desc()).to.be.equal('test');
+    expect(block.desc).to.be.equal('test');
   });
   
   it('.desc should set  desc to "test2"', function() {
     var block = flowy.createBlock('test');
-    block.desc('test2');
-    expect(block.desc()).to.be.equal('test2');
+    block.desc = 'test2';
+    expect(block.desc).to.be.equal('test2');
   });
 
   
@@ -38,95 +37,144 @@ describe('flowy', function() {
     var block = flowy.createBlock('test');
     flowy.pushBlock(block);
     var innerBlock = flowy.getBlock().tree[0];
-    expect(innerBlock.getObject().desc).to.be.equal('test');
+    expect(innerBlock.desc).to.be.equal('test');
   });
 
   it('Should create a inner block with desc "test"', function() {
     var block = flowy.createBlock('test');
     var innerBlock = flowy.createBlock('test2');
-    block.push(innerBlock);
+    block.tree.push(innerBlock);
     innerBlock = block.tree[0];
-    expect(innerBlock.desc()).to.be.equal('test2');
+    expect(innerBlock.desc).to.be.equal('test2');
   });
 
   it('Should create a statement block', function(){
     var block = flowy.statement('Initialise x = 0');
-    expect(block.tree[0].type()).to.be.equal('Statement');
-    expect(block.tree[0].desc()).to.be.equal('Initialise x = 0');
+    expect(block.tree[0].type).to.be.equal('Statement');
+    expect(block.tree[0].desc).to.be.equal('Initialise x = 0');
     
   });
 
   it('Should create a block and set it as current block', function(){
     var block = flowy.createBlock('block2');
     flowy.pushBlock(block, true);
-    expect(flowy.getBlock().desc()).to.be.equal('block2'); 
+    expect(flowy.getBlock().desc).to.be.equal('block2'); 
   });
 
   it('Should set top block as the current block', function(){
     var block = flowy.createBlock('block2');
     flowy.pushBlock(block, true);
     flowy.end();
-    expect(flowy.getBlock().desc()).to.be.equal('Begin'); 
+    expect(flowy.getBlock().desc).to.be.equal('Begin'); 
   });
 
   describe('Create simple if then do block', function() {
     it('Should create a If block and set top to current block', function(){
       flowy.if('x > 2');
-      expect(flowy.getBlock().type()).to.be.equal('If');
-      expect(flowy.getBlock().desc()).to.be.equal('x > 2');
+      expect(flowy.getBlock().type).to.be.equal('If');
+      expect(flowy.getBlock().desc).to.be.equal('x > 2');
     });
 
     it('Should create a If block and add steps in then block', function(){
       flowy.if('x > 2').then('x = x + 1');
-      expect(flowy.tree[0].type()).to.be.equal('Statement');
-      expect(flowy.tree[0].desc()).to.be.equal('x = x + 1');
+      expect(flowy.tree[0].type).to.be.equal('Statement');
+      expect(flowy.tree[0].desc).to.be.equal('x = x + 1');
     });
 
     it('Should create a If block and add steps in then block with empty block', function(){
       flowy.if('x > 2').then().do('x = x + 1');
-      expect(flowy.tree[0].type()).to.be.equal('Empty');
+      expect(flowy.tree[0].type).to.be.equal('Empty');
     });
 
-    it('Should create a If block and add steps in then block with empty block', function(){
+    it('Should create a If block and add steps in then block with Statement block', function(){
       flowy.if('x > 2').then().do('x = x + 1');
-      expect(flowy.tree[1].type()).to.be.equal('Statement');
-      expect(flowy.tree[1].desc()).to.be.equal('x = x + 1');
+      expect(flowy.tree[1].type).to.be.equal('Statement');
+      expect(flowy.tree[1].desc).to.be.equal('x = x + 1');
     });
 
     it('Should set top block to current block', function(){
       flowy.if('x > 2').then().do('x = x + 1').end();
-      expect(flowy.getBlock().desc()).to.be.equal('Begin');
-      expect(flowy.tree[0].type()).to.be.equal('If'); 
+      expect(flowy.getBlock().desc).to.be.equal('Begin');
+      expect(flowy.tree[0].type).to.be.equal('If'); 
     });
 
     it('Top block should be ElseIf block', function(){
       flowy.if('x > 2').then('x = x + 1').elseif('x = x + 2');
-      expect(flowy.getBlock().type()).to.be.equal('ElseIf');
-      expect(flowy.getBlock().desc()).to.be.equal('x = x + 2');
+      expect(flowy.getBlock().type).to.be.equal('ElseIf');
+      expect(flowy.getBlock().desc).to.be.equal('x = x + 2');
     });
 
-    it('Should create a ElseIf block and add steps in then block with empty block', function(){
+    it('Should create a ElseIf block and add steps in then block with Statement block', function(){
       flowy.if('x > 10').then().do('x = x + 1').elseif('x < 3').then('x = x - 1');
-      expect(flowy.tree[1].type()).to.be.equal('Statement');
-      expect(flowy.tree[1].desc()).to.be.equal('x = x - 1');
+      
+      expect(flowy.tree[0].type).to.be.equal('Statement');
+      expect(flowy.tree[0].desc).to.be.equal('x = x - 1');
     });
+
+    it('Should set top block to current block', function(){
+      flowy.if('x > 2').then().do('x = x + 1').else('x = x + 2').end();
+      expect(flowy.getBlock().desc).to.be.equal('Begin');
+      expect(flowy.tree[0].type).to.be.equal('If');
+      expect(flowy.tree[1].type).to.be.equal('Else'); 
+    })
 
     it('Top block should be Else block', function(){
       flowy.if('x > 2').then('x = x + 1').else('x = x + 2');
-      expect(flowy.getBlock().type()).to.be.equal('Else');
-      expect(flowy.getBlock().desc()).to.be.equal('x = x + 2');
+      expect(flowy.getBlock().type).to.be.equal('Else');
+      expect(flowy.getBlock().desc).to.be.equal('x = x + 2');
     });
 
     it('Should throw an exception', function(){ 
-      expect(flowy.else.bind(flowy.else,'x = x+2')).to.throw('Else without an If');
-      expect(flowy.elseif.bind(flowy.elseif,'x = x+2')).to.throw('Else without an If');
-      expect(flowy.if().else().else.bind(flowy.else,'x = x+2')).to.throw('Else without an If');
+      var flag = 0;
+      try{
+        flowy.else('x = x+2')
+      } catch(err) {
+        expect(err).to.be.eql('Else without an If');
+        flag |= 1;
+      }
+
+      try{
+        flowy.elseif('x = x+2')
+      } catch(err) {
+        expect(err).to.be.eql('Else without an If');
+        flag |= 2;
+      }
+
+      try{
+        flowy.if().else().else('x = x+2')
+      } catch(err) {
+        expect(err).to.be.eql('Else without an If');
+        flag |= 4;
+      }
+
+      expect(flag).to.be.equal(7);
     });
 
     it('Should not throw an exception', function(){ 
-      expect(flowy.if().else.bind(flowy.else,'x = x+2')).to.not.throw('Else without an If');
-      expect(flowy.if().elseif().elseif.bind(flowy.elseif,'x = x+2')).to.not.throw('Else without an If');
-      expect(flowy.if().elseif().else.bind(flowy.else,'x = x+2')).to.not.throw('Else without an If');
+      flowy.if().else();
+      flowy.if().elseif().elseif();
+      flowy.if().elseif().else();
+      flowy.if().elseif().then().elseif().do().else();
+    });
+
+    it('getJSON should print the entire block', function(){
+      flowy.if('x == 2').then('x = x + 1').elseif('x > 2').do('x = x + 2').else('x = x - 1');
+      expect(flowy.getJSON()).to.be.eql(
+        { tree:
+          [ { tree:
+              [ {desc: "x = x + 1", tree: [], type: "Statement"}],
+              desc: 'x == 2',
+              type: 'If'
+            },
+            { tree:
+              [{desc: "x = x + 2",tree: [],type: "Statement"}],
+              desc: 'x > 2',
+              type: 'ElseIf'
+            },
+            { tree: [], desc: 'x = x - 1', type: 'Else' } ],
+          desc: 'Begin',
+          type: 'Algorithm'
+        });
     });
     
   });
