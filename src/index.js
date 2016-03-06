ee3ea02d2660a1977ca91d13e7424fb267ed668e
Justin Jose <justinjose999@gmail.com>
2016-03-06 15:32:18 +0530

    Prepared test and created node package with dependance

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+
+module.exports = {
+  all: 1,
+  flowy: function(){
+
+  }
+};
---
+++ src/indexSpec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var flowy = require('./index');
+
+describe('flowy', function() {
+  it('init test', function() {
+    expect(true).to.be.true;
+  });
+});
